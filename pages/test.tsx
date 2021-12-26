ac556ae26ef578c32b9f6056beeb4e76b6ee005b
Matsuda1129 <69665638+Matsuda1129@users.noreply.github.com>
2021-12-26 20:47:15 +0900

    set poke

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,23 +1,11 @@
-import React, { useState } from 'react';
-import Avatar from 'react-avatar';
+export default function DisplayEnv({ pokemon }) {
+  return <div>{pokemon}</div>;
+}
 
-export default function Test() {
-  const imgurl =
-    'https://all-done-note-dev-picture-bucket.s3.ap-northeast-1.amazonaws.com/treeIcon.jpg';
-
-  const [icon, setIcon] = useState<File | null>(null);
-  const [previewIcon, setPreviewIcon] = useState<File | null>(null);
-
-  return (
-    <div>
-      <Avatar
-        size='160'
-        name='アイコン'
-        round
-        color='#ddd'
-        alt='アイコン'
-        src={imgurl}
-      />
-    </div>
-  );
+export async function getStaticProps() {
+  return {
+    props: {
+      pokemon: process.env.FAVORITE_POKEMON,
+    },
+  };
 }
