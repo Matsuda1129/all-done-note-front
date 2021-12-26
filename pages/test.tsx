0aa90396ab70468ae1099fba08ecb09c4f19983b
Matsuda1129 <69665638+Matsuda1129@users.noreply.github.com>
2021-12-26 20:51:45 +0900

    retry poke

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,11 +1,3 @@
-export default function DisplayEnv({ pokemon }) {
-  return <div>{pokemon}</div>;
-}
-
-export async function getStaticProps() {
-  return {
-    props: {
-      pokemon: process.env.FAVORITE_POKEMON,
-    },
-  };
+export default function DisplayEnv() {
+  return <div>{process.env.FAVORITE_POKEMON}</div>;
 }
