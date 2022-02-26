19ce4d7264ce35278138f4ad284f085c94faaf3d
Matsuda1129 <69665638+Matsuda1129@users.noreply.github.com>
2022-02-26 10:40:16 +0900

    fix s3method

--- pages/api/download-url.js
+++ pages/api/download-url.js
@@ -1,6 +1,12 @@
 import aws from 'aws-sdk';
 
 export default async function handler(req, res) {
+  aws.config.update({
+    accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_PUBLIC_SECRET_KEY,
+    region: process.env.NEXT_PUBLIC_REGION,
+    signatureVersion: 'v4',
+  });
   const key = req.query.file;
   const s3 = new aws.S3();
 
